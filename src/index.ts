add3ab84e8ecbee0baf37f45c569eb651d4dea78
owfdr <owfdr@ourworld.center>
2023-07-16 11:11:24 +0900

    feat: updating the database

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,14 @@ import { User } from "./entity/User";
   await AppDataSource.initialize();
   const userRepository = AppDataSource.getRepository(User);
 
-  console.log("All users: ", await userRepository.find());
+  const userToUpdate = await userRepository.findOneBy({
+    id: 1,
+  });
+  userToUpdate.firstName = "Timber";
+  await userRepository.save(userToUpdate);
 
   console.log(
-    "First user: ",
-    await userRepository.findOneBy({
-      id: 1,
-    })
+    "User has been updated: ",
+    await userRepository.findOneBy({ id: 1 })
   );
-
-  console.log(
-    "Robert: ",
-    await userRepository.findOneBy({
-      firstName: "Robert",
-    })
-  );
-
-  console.log(
-    "All male users: ",
-    await userRepository.findBy({
-      sex: "male",
-    })
-  );
-
-  const [allFemaleUsers, allFemaleUsersCount] =
-    await userRepository.findAndCountBy({
-      sex: "female",
-    });
-  console.log("All female users: ", allFemaleUsers);
-  console.log("Female users count: ", allFemaleUsersCount);
 })();
