42db3cb5fcd97e7333ba96d7a34ebf99b9f0a203
owfdr <owfdr@ourworld.center>
2023-07-16 13:09:31 +0900

    feat: load with relations

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import Factory from "./class/Factory";
 import { User } from "./entity/User";
-import { UserProfile } from "./entity/UserProfile";
 import { AppDataSource } from "./data-source";
 
 (async () => {
   await AppDataSource.initialize();
   const userRepository = AppDataSource.getRepository(User);
-  const userProfileRepository = AppDataSource.getRepository(UserProfile);
 
-  const userProfile = Factory.userProfile();
-  const user = Factory.user();
+  // using find method
+  console.log(
+    "All users: ",
+    await userRepository.find({
+      relations: ["profile"],
+    })
+  );
 
-  // user.profile = userProfile;
-  userProfile.user = user;
-
-  await userRepository.save(user);
-  await userProfileRepository.save(userProfile); // This order matters
-
-  const userWithProfile = await userRepository.find({ relations: ["profile"] });
-  console.log(userWithProfile);
+  // using query builder
+  console.log(
+    "All users: ",
+    await userRepository
+      .createQueryBuilder("user")
+      .leftJoinAndSelect("user.profile", "profile")
+      .getMany()
+  );
 })();
