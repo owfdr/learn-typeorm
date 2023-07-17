cc418e156a55a3cc9a1946b169fba4ff6a579f38
owfdr <owfdr@ourworld.center>
2023-07-17 10:07:32 +0900

    feat: import all entities

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,6 @@
 import "reflect-metadata";
 import path = require("path");
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
-import { UserProfile } from "./entity/UserProfile";
-import { Image } from "./entity/Image";
-import { Todo } from "./entity/Todo";
-import { Tag } from "./entity/Tag";
 
 const sqlitePath = path.resolve(__dirname, "../database.sqlite");
 
@@ -14,7 +9,7 @@ export const AppDataSource = new DataSource({
   database: sqlitePath,
   synchronize: true,
   logging: false,
-  entities: [User, UserProfile, Image, Todo, Tag],
+  entities: ["src/entity/*.ts"],
   migrations: [],
   subscribers: [],
 });
